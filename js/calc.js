81a5c8a844a4ca227573380d9df92927acf7169e
Nicole Deltoro <geekwise.Nicole.deltoro@gmail.com>
2016-04-15 12:02:40 -0700

    edit

--- js/calc.js
+++ js/calc.js
@@ -2,16 +2,17 @@ var FKeyPad = document.Keypad;
 var Accumulate = 0;
 var FlagNewNum = false;
 var PendingOp = "";
-   function NumPressed (Num) {
-         if (FlagNewNum) {
-         FKeyPad.ReadOut.value  = Num;
-         FlagNewNum = false;
+
+function NumPressed (Num) {
+    if (FlagNewNum) {
+        FKeyPad.ReadOut.value  = Num;
+        FlagNewNum = false;
    }
-            else {
-            if (FKeyPad.ReadOut.value == "0")
+    else {
+        if (FKeyPad.ReadOut.value == "0")
             FKeyPad.ReadOut.value = Num;
-               else
-                  FKeyPad.ReadOut.value += Num;
+        else
+            FKeyPad.ReadOut.value += Num;
          }
       }
       
@@ -19,47 +20,46 @@ function Operation (Op) {
    var Readout = FKeyPad.ReadOut.value;
    if (FlagNewNum && PendingOp != "=");
       else
-{
-   FlagNewNum = true;
-      if ( '+' == PendingOp )
-         Accumulate += parseFloat(Readout);
-         else if ( '-' == PendingOp )
-         Accumulate -= parseFloat(Readout);
-         else if ( '/' == PendingOp )
-         Accumulate /= parseFloat(Readout);
-         else if ( '*' == PendingOp )
-         Accumulate *= parseFloat(Readout);
+      {
+          FlagNewNum = true;
+    if ( '+' == PendingOp )
+        Accumulate += parseFloat(Readout);
+            else if ( '-' == PendingOp )
+        Accumulate -= parseFloat(Readout);
+            else if ( '/' == PendingOp )
+        Accumulate /= parseFloat(Readout);
+            else if ( '*' == PendingOp )
+        Accumulate *= parseFloat(Readout);
             else
-               Accumulate = parseFloat(Readout);
-               FKeyPad.ReadOut.value = Accumulate;
-               PendingOp = Op;
+        Accumulate = parseFloat(Readout);
+        FKeyPad.ReadOut.value = Accumulate;
+        PendingOp = Op;
          }
    }
    
 function Decimal () {
-   var curReadOut = FKeyPad.ReadOut.value;
+    var curReadOut = FKeyPad.ReadOut.value;
       if (FlagNewNum) {
-      curReadOut = "0.";
-      FlagNewNum = false;
+        curReadOut = "0.";
+        FlagNewNum = false;
 }
-      else
-   {
-      if (curReadOut.indexOf(".") == -1)
-      curReadOut += ".";
-   }
-   
-         FKeyPad.ReadOut.value = curReadOut;
+        else
+{
+    if (curReadOut.indexOf(".") == -1)
+        curReadOut += ".";
+        }
+   FKeyPad.ReadOut.value = curReadOut;
 }
    
-   function ClearEntry () {
-      FKeyPad.ReadOut.value = "0";
-      FlagNewNum = true;
-   }
+function ClearEntry () {
+    FKeyPad.ReadOut.value = "0";
+    FlagNewNum = true;
+}
    
-   function Clear () {
-      Accumulate = 0;
-      PendingOp = "";
-   ClearEntry();
+function Clear () {
+    Accumulate = 0;
+    PendingOp = "";
+    ClearEntry();
 }
 
 function Neg () {
